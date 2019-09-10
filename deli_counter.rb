28a240f605d112c030dfeff9c2722325acc88100
Danny Buckley <dannyboy.msn@hotmail.com>
2019-09-10 12:34:47 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,6 +2,6 @@
 katz_deli = []
 
 def line
- outputs = katz_deli.empty? "The line is currently empty." : katz_deli
+ outputs = katz_deli.empty? ? "The line is currently empty." : katz_deli
   puts outputs
 end
