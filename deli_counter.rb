814139d43a8b7a778174514b0606d69c53a16b87
Danny Buckley <dannyboy.msn@hotmail.com>
2019-09-10 12:43:49 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,6 +2,15 @@ require 'pry'
 katz_deli = []
 # binding.pry
 def line(array)
- puts array.empty? ? "The line is currently empty." : array
 
+ if array.empty?
+   puts "The line is currently empty."
+ else
+   string = "The line is currently:"
+
+   array.each_with_index do |name, index|
+     string << " #{index + 1}. #{name}"
+   end
+   puts string
+ end
 end
